746eb0f27a1d90a7eb09db99d991d195cb77c2f1
sakthivelpandiyan2264 <sakthivelpandiyan2264@gmail.com>
2024-03-09 15:04:10 +0530

    sixth commit

--- src/pages/Category/Categories.jsx
+++ src/pages/Category/Categories.jsx
@@ -24,11 +24,11 @@ const Categories = () => {
 
   // Array of descriptions corresponding to each category
   const details = [
-    "Health & Safety",
-    "Description for Adulthood",
-    "Description for Pregnant Women",
-    "Description for Middle Age",
-    "Description for Old Age"
+    "Health & Safety, Healthcare & Hygiene, Clothings, sleep & Bedding",
+    "Health&Wellness, Personal Style, Career & Professional development, Education & Personal Growth ",
+    "Prenatal health, Comfort & Clothing, Health & Wellness, Preparation for Baby, Childbirth Preparation",
+    "Self-Care & Beauty, Career & Person Development, Financial Management, Fitness & Exercise, Mental Health",
+    "Medical Care, Comfortable & Adaptive Living, Financial Management, End of Life Planning"
   ];
 
   return (
@@ -38,15 +38,15 @@ const Categories = () => {
         <div className="container  py-20 mx-auto my-1o flex justify-center items-center gap-20 ">
           <ul className="space-y-4 flex  gap-5">
             {["Childhood", "Adulthood", "Pregnant Women", "Middle Age", "Old Age"].map((item, index) => (
-              <li key={index} className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <li key={index} className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 ">
                 <button
-                  className="bg-indigo-700 text-white text-xl px-3 py-2 rounded-2xl hover:bg-purple-900 transition-colors duration-300 drop-shadow-lg"
+                  className="bg-indigo-700 text-white text-xl px-3 py-3 rounded-2xl hover:bg-purple-900 transition-colors duration-300 drop-shadow-lg"
                   onClick={() => handleButtonClick(item)}
                 >
                   {item}
                 </button>
                 {/* Access the corresponding description using index */}
-                <p className="mb-3 py-2 font-normal text-gray-700 dark:text-gray-400">{details[index]}</p>
+                <p className=" py-4 font-normal text-gray-700 dark:text-gray-400">{details[index]}</p>
               </li>
             ))}
           </ul>
