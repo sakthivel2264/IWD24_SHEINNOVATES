250fbdcf015dd2579cc039903299412b3fbaa7d4
sakthivelpandiyan2264 <sakthivelpandiyan2264@gmail.com>
2024-03-09 14:46:37 +0530

    fifth commit

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 const Layout = ({ children }) => {
   return (
-    <div className="bg-gradient-to-r from-violet-500 to-fuchsia-500 min-h-screen flex justify-center  items-start">
+    <div className="bg-black/90 min-h-screen flex justify-center  items-start">
       <div className="mx-64  flex flex-col justify-center items-center">
         {children}
       </div>
--- src/pages/Category/Categories.jsx
+++ src/pages/Category/Categories.jsx
@@ -1,11 +1,10 @@
 import Header from "../../components/Header";
 import Layout from "../../layout/Layout";
-import Footer from "../../components/Footer"
+import Footer from "../../components/Footer";
 
 const Categories = () => {
   
   const handleButtonClick = (item) => {
-    
     if (item === "Childhood") {
       window.location.href = "/childhood";
     }
@@ -20,29 +19,40 @@ const Categories = () => {
     }
     if (item === "Old Age") {
       window.location.href = "/senioryears";
-  }
-};
+    }
+  };
+
+  // Array of descriptions corresponding to each category
+  const details = [
+    "Health & Safety",
+    "Description for Adulthood",
+    "Description for Pregnant Women",
+    "Description for Middle Age",
+    "Description for Old Age"
+  ];
 
   return (
     <>
-    <Layout>
-      <Header />
-      <div className="container  py-40 mx-auto my-1o flex justify-center items-center gap-20 flex-col">
-        <ul className="space-y-4  gap-20">
-          {["Childhood", "Adulthood", "Pregnant Women", "Middle Age", "Old Age"].map((item, index) => (
-            <li key={index}>
-              <button
-                className="bg-indigo-700 text-white text-xl px-3 py-2 rounded-2xl hover:bg-purple-900 transition-colors duration-300 drop-shadow-lg"
-                onClick={() => handleButtonClick(item)}
-              >
-                {item}
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </Layout>
-    <Footer />
+      <Layout>
+        <Header />
+        <div className="container  py-20 mx-auto my-1o flex justify-center items-center gap-20 ">
+          <ul className="space-y-4 flex  gap-5">
+            {["Childhood", "Adulthood", "Pregnant Women", "Middle Age", "Old Age"].map((item, index) => (
+              <li key={index} className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <button
+                  className="bg-indigo-700 text-white text-xl px-3 py-2 rounded-2xl hover:bg-purple-900 transition-colors duration-300 drop-shadow-lg"
+                  onClick={() => handleButtonClick(item)}
+                >
+                  {item}
+                </button>
+                {/* Access the corresponding description using index */}
+                <p className="mb-3 py-2 font-normal text-gray-700 dark:text-gray-400">{details[index]}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </Layout>
+      <Footer />
     </>
   );
 };
